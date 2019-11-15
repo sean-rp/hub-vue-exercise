2ff477d2e0a9c7430e0dcf6fc2f6698dcf6f22e2
Sean Doe-Simkins <sdoesimkins@rightpoint.com>
2019-11-15 08:04:02 -0600

    WIP

--- src/components/ViewContacts.vue
+++ src/components/ViewContacts.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
-    <b-table striped hover @row-clicked="rowClick" :items="items"></b-table>
+    <b-table striped hover @row-clicked="rowClick" :items="contacts"></b-table>
   </div>
 </template>
 
 <script>
+// import axios from 'axios'
+
 export default {
   methods: {
     rowClick(row) {
       this.$router.push({ path: `/edit/${row.id}` })
     }
   },
+  // props: ['contacts'],
   data() {
     return {
-      items: [
-        { id: 1, firstName: 'Dickerson', lastName: 'Macdonald' },
-      ]
+      contacts: this.contacts
     }
-  }
+  },
+  mounted () {
+    // axios
+    //   .get('https://jsonplaceholder.typicode.com/users')
+    //   .then(response => {
+    //     this.contacts = response.data.map(contact => {
+    //       return {
+    //         id: contact.id,
+    //         name: contact.name,
+    //         street: contact.address.street,
+    //         city: contact.address.city,
+    //         zip: contact.address.zipcode,
+    //         email: contact.email,
+    //         phone: contact.phone,
+    //       }
+    //     })
+    //   })
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -16,14 +16,14 @@ Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 const routes = [
-  { path: '/', component: ViewContacts },
+  { path: '/', component: ViewContacts, props: true },
   { path: '/create', component: CreateContact },
   // TODO can these be combined since they're the same form and then conditionally render create or edit?
   { path: '/edit/:id', component: EditContact },
 ]
 
 const router = new VueRouter({
-  routes // short for `routes: routes`
+  routes
 })
 
 new Vue({
@@ -35,7 +35,19 @@ new Vue({
   mounted () {
     axios
       .get('https://jsonplaceholder.typicode.com/users')
-      .then(response => (this.contacts = response))
+      .then(response => {
+        this.contacts = response.data.map(contact => {
+          return {
+            id: contact.id,
+            name: contact.name,
+            street: contact.address.street,
+            city: contact.address.city,
+            zip: contact.address.zipcode,
+            email: contact.email,
+            phone: contact.phone,
+          }
+        })
+      })
   },
   router,
   render: h => h(App),
